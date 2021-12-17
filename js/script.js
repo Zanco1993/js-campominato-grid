fde27c26dd4266766727766a2976d3602e95a7cd
Zanco1993 <zanconatofabio@gmail.com>
2021-12-17 16:21:49 +0100

    create le funzioni per la griglia

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*
 Consegna
-L’utente indica un livello di difficoltà (3 pulsanti) in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+L’utente indica un livello di difficoltà (3 pulsanti) in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
@@ -8,27 +9,25 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
 function createGrillEasy(max){
-    buttonEasy.addEventListener('click', function(){
-        for (let i = 0; i < max; i++) {
+   
+        for (let i = 1; i <= max; i++) {
             createNewBox(gridContainerHtml);
         }
-    })
-}
+    }
+
 
 function createGrillMedium(max){
-    buttonEasy.addEventListener('click', function(){
-        for (let i = 0; i < max; i++) {
+        for (let i = 1; i <= max; i++) {
             createNewBox(gridContainerHtml);
         }
-    })
+
 }
 
 function createGrillHard(max){
-    buttonEasy.addEventListener('click', function(){
-        for (let i = 0; i < max; i++) {
+        for (let i = 1; i <= max; i++) {
             createNewBox(gridContainerHtml);
         }
-    })
+    
 }
 
 function createNewBox(container) {
@@ -43,10 +42,18 @@ function createNewBox(container) {
 
 
 const gridContainerHtml = document.querySelector('.grid-container');
-const buttonEasy = document.querySelector('easy-difficult');
-const buttonMedium = document.querySelector('medium-difficult');
-const buttonHard = document.querySelector('hard-difficult');
+const buttonEasy = document.querySelector('.easy-difficult');
+const buttonMedium = document.querySelector('.medium-difficult');
+const buttonHard = document.querySelector('.hard-difficult');
 
 buttonEasy.addEventListener('click', function(){
     createGrillEasy(100);
+})
+
+buttonMedium.addEventListener('click', function(){
+    createGrillEasy(81);
+})
+
+buttonHard.addEventListener('click', function(){
+    createGrillEasy(49);
 })
